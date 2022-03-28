4dd3f620731d0d0ea40d0f2e5878aaaefd2fbfec
NsamaChibulu <nsama.chibulu@outlook.com>
2022-03-28 17:48:48 +0100

    bubble sort

---
+++ Sorting/Bubble.py
@@ -0,0 +1,18 @@
+#   Code implementation for bubble sort 
+
+class Bubble:
+    #definifn the function Bubble sort
+    def bubble_sort(list): 
+        unsorted_until_index = len(list) - 1
+        sorted = False
+
+        while not sorted:
+            sorted = True
+            for i in range(unsorted_until_index):
+                if list[i] > list[i+1]:
+                    list[i], list[i+1] = list[i+1], list[i]
+                    sorted = False
+            unsorted_until_index -= 1
+        return list 
+
+    print(bubble_sort([3, 6, 9, 2, 1]))    
